f2d9fb0c4dc0fdeec915d02c62ca0ea85ea89d1a
Robin Fishman <rfishman@csumb.edu>
2024-06-06 17:38:28 -0700

    [Trial] Reconvert Dataframe into primitive list to allow experimentation

---
+++ trial/sample_points_processing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 09:57:15 2024
+
+@author: robin
+"""
+import pandas as pd
+
+# First see if I can get one table to work
+sample_points_forecast = pd.read_csv("sample_points_forecast.csv", low_memory=False)
+
+sample_points_forecast.drop('Unnamed: 0', axis=1, inplace=True)
+
+spf_dict = sample_points_forecast.to_dict(orient='list')
