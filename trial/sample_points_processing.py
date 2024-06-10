47dad289ba6961d6c3cde2dc716b6c7b84564e8d
Robin Fishman <rfishman@csumb.edu>
2024-06-10 09:52:23 -0700

    [Trial] Data gathering for 6/10

--- trial/sample_points_processing.py
+++ trial/sample_points_processing.py
@@ -12,3 +12,7 @@ sample_points_forecast = pd.read_csv("sample_points_forecast.csv", low_memory=Fa
 sample_points_forecast.drop('Unnamed: 0', axis=1, inplace=True)
 
 spf_dict = sample_points_forecast.to_dict(orient='list')
+
+# Ideas
+# - During generation, use the .content 'time' field as a multi-index. Use 'et' field for data value
+#                  - .content field e.g.  [{time: '01-01-2023', 'et': 15.0}, ...]
