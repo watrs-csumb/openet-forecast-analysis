8f365499dea65d4f8fa4f115a7fd9d750083643b
Robin Fishman <rfishman@csumb.edu>
2024-06-13 15:54:37 -0700

    Notes on concurrency checks

--- sample_points_main.py
+++ sample_points_main.py
@@ -19,3 +19,20 @@ timeseries_endpoint = "https://developer.openet-api.org/raster/timeseries/point"
 forecast_endpoint = "https://developer.openet-api.org/experimental/raster/timeseries/forecasting/seasonal"
 
 header = {"Authorization": dotenv_values(".env").get("ET_KEY")}
+
+"""
+Concurrency approaches:
+	1) Simplest: in a while loop for each attempt, use try-except statement until loop ends
+	2) Organic: in a try-except statement:
+		a) Make attempt
+		b) If requests.exceptions.RequestException, start a timer
+			- This requires new method with (func, attempts) params
+			- Timeout is exponential (e.g. 2 seconds, 20 seconds, 200 seconds)
+		c) Allow the option of manually reattempting
+			- Prompt in terminal or GUI
+		d) Reattempts fetch from a queue of field IDs
+			- A field ID is only removed *after* a successful attempt
+    3) Researched: using HTTPAdapter & urllib3 Retry libraries
+		a) Start an HTTP Session
+		b) Create a retry strategy and mount it to the session
+""" 
