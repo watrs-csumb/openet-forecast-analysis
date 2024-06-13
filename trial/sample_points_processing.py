46a99a974eff83017c0e09eaff3c625ef7775241
Robin Fishman <rfishman@csumb.edu>
2024-06-13 10:00:17 -0700

    [Trial] Create dataframe for actual ET data for sample points

--- trial/sample_points_processing.py
+++ trial/sample_points_processing.py
@@ -9,20 +9,15 @@ import pandas as pd
 
 # First see if I can get one table to work
 sample_points_forecast = pd.read_csv("sample_points_forecast.csv", low_memory=False)
-
 sample_points_forecast.drop('Unnamed: 0', axis=1, inplace=True)
 
 spf_dict = sample_points_forecast.to_dict(orient='list')
 
-# Ideas
-# - During generation, use the .content 'time' field as a multi-index. Use 'et' field for data value
-#                  - .content field e.g.  [{time: '01-01-2023', 'et': 15.0}, ...]
-
-sample_df = {
-    'field': [],
+sample_forecast_df = {
+    'field_id': [],
     'time': [],
-    'data': []
-    }
+    'et_expected_mm': []
+}
 
 for entry in spf_dict:
     sample_entry = spf_dict[entry]
@@ -31,9 +26,31 @@ for entry in spf_dict:
         # Convert data into a dict using safer eval() method
         data = ast.literal_eval(data)
         
-        sample_df['field'].append(entry)
-        sample_df['time'].append(data['time'])
-        sample_df['data'].append(data['et'])
+        sample_forecast_df['field_id'].append(entry)
+        sample_forecast_df['time'].append(data['time'])
+        sample_forecast_df['et_expected_mm'].append(data['et'])
 
-sample_df = pd.DataFrame(sample_df)
+sample_forecast_df = pd.DataFrame(sample_forecast_df)
 
+sample_points_timeseries = pd.read_csv("sample_points_timeseries.csv", low_memory=False)
+sample_points_timeseries.drop('Unnamed: 0', axis=1, inplace=True)
+
+spt_dict = sample_points_timeseries.to_dict(orient='list')
+
+sample_timeseries_df = {
+    'field_id': [],
+    'time': [],
+    'et_actual_mm': []
+}
+
+for entry in spt_dict:
+    sample_entry = spt_dict[entry]
+    
+    for data in sample_entry:
+        data = ast.literal_eval(data)
+        
+        sample_timeseries_df['field_id'].append(entry)
+        sample_timeseries_df['time'].append(data['time'])
+        sample_timeseries_df['et_actual_mm'].append(data['et'])
+
+sample_timeseries_df = pd.DataFrame(sample_timeseries_df)
