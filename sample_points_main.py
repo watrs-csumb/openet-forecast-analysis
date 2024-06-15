8bdb035843dc1bef128d684a398c661012d57ea8
Robin Fishman <rfishman@csumb.edu>
2024-06-15 02:07:20 -0700

    [Samples] Move samples code into own main function - declutter global scope

--- sample_points_main.py
+++ sample_points_main.py
@@ -16,8 +16,11 @@ forecast_endpoint = "https://developer.openet-api.org/experimental/raster/timese
 sample_points_reference = pd.read_csv('sample_points.csv', low_memory=False).set_index('OPENET_ID')
 sample_points_queue = Queue(sample_points_reference.index.to_list())
 
-if __name__ == '__main__':
+def main():
 	sample_data = ETPreprocess(sample_points_queue, sample_points_reference)
 	failed_attempts = sample_data.start(timeseries_endpoint, forecast_endpoint)
-	
+ 
 	print("[LOG] Finished processing. " + str(failed_attempts) + " fields failed.")
+
+if __name__ == '__main__':
+	main()	
