de93d4438261cc5c6afbb8ff8e2391aaf35c4dd2
Robin Fishman <rfishman@csumb.edu>
2024-06-30 18:04:24 -0700

    [Samples] Add StreamHandler to logger. Displays warnings and errors to terminal.

--- sample_points_main.py
+++ sample_points_main.py
@@ -10,10 +10,21 @@ from Queue import Queue
 
 import logging
 import pandas as pd
+import sys
 
-logging.basicConfig(level=logging.INFO, filename=datetime.now().strftime(f'logs/sample_points_main_%Y_%m_%d_%H_%M_%S.log'),
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+# LOGGING CONFIG
+# File handler that allows files to show all log entries
+file_log_handler = logging.FileHandler(filename=datetime.now().strftime(f'logs/sample_points_main_%Y_%m_%d_%H_%M_%S.log'))
+
+# Stream handler that prints log entries at level WARNING or higher
+stdout_log_handler = logging.StreamHandler(stream=sys.stdout)
+stdout_log_handler.setLevel(logging.WARNING)
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[file_log_handler, stdout_log_handler])
 logger = logging.getLogger(__name__)
+# END LOGGING CONFIG
 
 timeseries_endpoint = "https://developer.openet-api.org/raster/timeseries/point"
 forecast_endpoint = "https://developer.openet-api.org/experimental/raster/timeseries/forecasting/seasonal"
@@ -28,7 +39,7 @@ def main():
  
 	logger.info(f"Finished processing. {str(failed_attempts)} fields failed.")
 	# logger.info("\n" + sample_data.data_table.to_string().replace('\n', '\n\t'))
-	sample_data.data_table.to_csv("samples_data_table.csv")
+	# sample_data.data_table.to_csv("samples_data_table.csv")
 
 if __name__ == '__main__':
 	main()
