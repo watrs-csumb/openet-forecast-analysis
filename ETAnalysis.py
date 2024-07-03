9f6222611ea9edb25e4012be0d80293bf38892a0
Robin Fishman <rfishman@csumb.edu>
2024-07-02 23:11:49 -0700

    [ET] Add metric function

--- ETAnalysis.py
+++ ETAnalysis.py
@@ -1,3 +1,23 @@
-import numpy
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+import numpy as np
 import pandas as pd
 import scipy
+
+def calculate_metrics(data: pd.DataFrame) -> pd.Series:
+    if ['et_actual', 'et_forecast'] not in data.columns:
+        raise ValueError
+    mae = mean_absolute_error(data['et_actual'], data['et_forecast'])
+    mse = mean_squared_error(data['et_actual'], data['et_forecast'])
+    rmse = np.sqrt(mse)
+    cor = np.corrcoef(data['et_actual'], data['et_forecast'])[0, 1]
+    bias = np.mean(data['et_actual'] - data['et_forecast'])
+    skill_score = 1 - (mse / mean_squared_error(data['et_actual'], [np.mean(data['et_forecast'])] * len(data)))
+    
+    return pd.Series({
+		'mae': mae,
+		'rmse': rmse,
+		'corr': cor,
+		'bias': bias,
+		'skill_score': skill_score
+	})
