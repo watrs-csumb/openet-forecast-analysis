5741aab16c10128ab2d6ff6c832a4cb59559d811
Robin Fishman <rfishman@csumb.edu>
2024-07-11 14:54:35 -0700

    [Samples] Code for forecasting the year starting at weekly intervals.

--- sample_points_main.py
+++ sample_points_main.py
@@ -4,7 +4,7 @@ Created on Thu Jun  13 10:06:44 2024
 
 @author: Robin Fishman
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 from ETArg import ETArg
 from ETPreprocess import ETPreprocess
 from Queue import Queue
@@ -22,8 +22,8 @@ stdout_log_handler = logging.StreamHandler(stream=sys.stdout)
 stdout_log_handler.setLevel(logging.WARNING)
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[file_log_handler, stdout_log_handler])
+					format='%(asctime)s - %(levelname)s - %(message)s',
+					handlers=[file_log_handler, stdout_log_handler])
 logger = logging.getLogger(__name__)
 # END LOGGING CONFIG
 
@@ -37,51 +37,67 @@ sample_points_queue = Queue(sample_points_reference.index.to_list())
 def main():
 	sample_data = ETPreprocess(sample_points_queue, sample_points_reference)
  
+	# Gather predictions at weekly intervals.
+	# Forecast begins predictions from the end_range. So to start predictions for Jan 1, set to Dec 31
+	forecasting_date = datetime(2023, 12, 31) # Marker for loop
+	end_date = datetime(2024, 12, 31) # 31 Dec 2024
+	interval_delta = timedelta(weeks=1) # weekly interval
+	while forecasting_date < end_date:
+		api_date_format = forecasting_date.strftime('%Y-%m-%d')	
+		filename = f"data/forecasts/{api_date_format}_forecast.csv"
+  
+		forecast_et = ETArg('expected_et', args={
+			'endpoint': forecast_endpoint,
+			'date_range': ['2016-01-01', api_date_format],
+			'variable': 'ET'
+		})
+  
+		forecast_eto = ETArg('expected_eto', args={
+			'endpoint': forecast_endpoint,
+			'date_range': ['2016-01-01', api_date_format],
+			'variable': 'ETo'
+		})
+  
+		forecast_etof = ETArg('expected_etof', args={
+			'endpoint': forecast_endpoint,
+			'date_range': ['2016-01-01', api_date_format],
+			'variable': 'ETof'
+		})
+  
+		sample_data.start(request_args=[
+			forecast_et, forecast_eto, forecast_etof
+		], frequency="daily", logger=logger)
+  
+		sample_data.export(filename)
+		
+		forecasting_date = forecasting_date + interval_delta
+ 
 	timeseries_et = ETArg('actual_et', args={
 		'endpoint': timeseries_endpoint,
-		'date_range': ['2016-01-01', '2024-07-07'],
-		'variable': 'ET'
-	})
- 
-	forecast_et = ETArg('expected_et', args={
-		'endpoint': forecast_endpoint,
-		'date_range': ['2016-01-01', '2024-07-14'],
+		'date_range': ['2016-01-01', '2023-12-31'],
 		'variable': 'ET'
 	})
  
 	timeseries_eto = ETArg('actual_eto', args={
 		'endpoint': timeseries_endpoint,
-		'date_range': ['2016-01-01', '2024-07-07'],
-		'variable': 'ETo'
-	})
- 
-	forecast_eto = ETArg('expected_eto', args={
-		'endpoint': forecast_endpoint,
-		'date_range': ['2016-01-01', '2024-07-14'],
+		'date_range': ['2016-01-01', '2023-12-31'],
 		'variable': 'ETo'
 	})
  
 	timeseries_etof = ETArg('actual_etof', args={
 		'endpoint': timeseries_endpoint,
-		'date_range': ['2016-01-01', '2024-07-07'],
+		'date_range': ['2016-01-01', '2023-12-31'],
 		'variable': 'ETof'
 	})
  
-	forecast_etof = ETArg('expected_etof', args={
-		'endpoint': forecast_endpoint,
-		'date_range': ['2016-01-01', '2024-07-14'],
-		'variable': 'ETof'
-	}) 
- 
-	failed_attempts = sample_data.start(request_args=[
+	sample_data.start(request_args=[
 		timeseries_et, 
-     	timeseries_eto, 
-      	timeseries_etof
-      ], frequency='daily', logger=logger)
+	 	timeseries_eto, 
+	  	timeseries_etof
+	  ], frequency='daily', logger=logger)
  
-	logger.info(f"Finished processing. {str(failed_attempts)} fields failed.")
 	# logger.info("\n" + sample_data.data_table.to_string().replace('\n', '\n\t'))
-	sample_data.data_table.to_csv("data/samples_historical_data_10y.csv")
+	sample_data.export("data/samples_historical_data_10y.csv")
 
 if __name__ == '__main__':
 	main()
