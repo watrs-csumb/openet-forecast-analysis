8a1432d6550ddadfad2068ff5f38fdbb76f95b65
Robin Fishman <rfishman@csumb.edu>
2024-10-08 19:01:04 +0000

    [FRET] Fetch polygons for all fields. Migrate FRET to polygon fetch

--- fret_auto_main.py
+++ fret_auto_main.py
@@ -20,7 +20,7 @@ import time
 # LOGGING CONFIG
 # File handler that allows files to show all log entries
 file_log_handler = logging.FileHandler(
-	filename=datetime.now().strftime(f'logs/f{os.path.basename(__file__)}_%Y_%m_%d_%H_%M_%S.log')
+	filename=datetime.now().strftime(f'logs/{os.path.basename(__file__)}_%Y_%m_%d_%H_%M_%S.log')
 )
 
 # Stream handler that prints log entries at level WARNING or higher
@@ -36,15 +36,15 @@ logger = logging.getLogger(__name__)
 # END LOGGING CONFIG
 
 fret_endpoint = "https://developer.openet-api.org/experimental/raster/timeseries/forecasting/fret"
-forecast_endpoint = "https://developer.openet-api.org/experimental/raster/timeseries/forecasting/seasonal"
-timeseries_endpoint = "https://developer.openet-api.org/raster/timeseries/point"
+forecast_endpoint = "https://developer.openet-api.org/experimental/raster/timeseries/forecasting/seasonal_polygon"
+timeseries_endpoint = "https://developer.openet-api.org/raster/timeseries/polygon"
 
 api_key = dotenv_values(".env").get("ET_KEY")
-kern_fields = pd.read_csv("./data/Kern.csv", low_memory=False).set_index("OPENET_ID")
-monterey_fields = pd.read_csv("./data/Monterey.csv", low_memory=False).set_index("OPENET_ID")
+kern_fields = pd.read_csv("./data/kern_polygons.csv", low_memory=False).set_index("OPENET_ID")
+monterey_fields = pd.read_csv("./data/monterey_polygons.csv", low_memory=False).set_index("OPENET_ID")
 
 def main():
-	# Plan is to auto fetch FRET data every 6 days from script start. Script is to run continuously, checking for this time interval each minute.
+    # Plan is to auto fetch FRET data every 6 days from script start. Script is to run continuously, checking for this time interval each minute.
     check_time = datetime.now()
     check_interval = timedelta(days=6)
     upcoming_check_time = check_time + check_interval
@@ -53,93 +53,126 @@ def main():
     monterey_queue = Queue(monterey_fields.index.to_list())
     kern_queue = Queue(kern_fields.index.to_list())
 
-    et_arg = ETArg("fret_et", args={"endpoint": fret_endpoint, "variable": "ET"})
     forecast_eto = ETArg(
-        "expected_eto",
+        "dtw_eto",
         args={
             "endpoint": forecast_endpoint,
             "date_range": ["2016-01-01", check_time.strftime("%Y-%m-%d")],
-            "variable": "ETo"
+            "variable": "ETo",
+            "reducer": "mean"
         },
     )
-    eto_arg = ETArg("fret_eto", args={"endpoint": fret_endpoint, "variable": "ETo"})
-    etof_arg = ETArg("fret_etof", args={"endpoint": fret_endpoint, "variable": "ETof"})
+    eto_arg = ETArg(
+        "fret_eto",
+        args={
+            "endpoint": timeseries_endpoint, 
+            "variable": "ETo", 
+            "reference": "fret", 
+            "date_range": [datetime.now().strftime("%Y-%m-%d"), (datetime.now() + timedelta(weeks=1)).strftime("%Y-%m-%d")],
+            "reducer": "mean"
+        },
+    )
 
     logger.info(f"FRET automation started on: {check_time}")
     try:
         while True:
-			# This method performs a do-while loop. Initially running the FRET data fetch, and does so when the run_fetch toggle is True.
+            # This method performs a do-while loop. Initially running the FRET data fetch, and does so when the run_fetch toggle is True.
             if run_fetch is True:
                 export_date_format = check_time.strftime("%Y-%m-%d")
-                monterey_fret = ETPreprocess(deepcopy(monterey_queue), monterey_fields, api_key=api_key) # type: ignore
-                monterey_fret.start(
-					request_args=[eto_arg],
-					logger=logger,
-					packets=True
-				)
-                monterey_fret.export(f"data/forecasts/fret/monterey_fret_{export_date_format}.csv")
+                monterey_fret = ETPreprocess(
+                    deepcopy(monterey_queue), monterey_fields, api_key=api_key
+                )  # type: ignore
+                monterey_fret.start(request_args=[forecast_eto, eto_arg], logger=logger, packets=True, frequency='daily')
+                monterey_fret.export(
+                    f"data/forecasts/fret/monterey_fret_{export_date_format}.csv"
+                )
 
-                kern_fret = ETPreprocess(deepcopy(kern_queue), kern_fields, api_key=api_key) # type: ignore
-                kern_fret.start(
-					request_args=[eto_arg],
-					logger=logger,
-					packets=True
-				)
-                kern_fret.export(f"data/forecasts/fret/kern_fret_{export_date_format}.csv")
+
+                kern_fret = ETPreprocess(
+                    deepcopy(kern_queue), kern_fields, api_key=api_key
+                )  # type: ignore
+                kern_fret.start(request_args=[forecast_eto, eto_arg], logger=logger, packets=True, frequency='daily')
+                kern_fret.export(
+                    f"data/forecasts/fret/kern_fret_{export_date_format}.csv"
+                )
                 logger.info(
-					f"FRET fetched on: {check_time}. Next check will be on: {upcoming_check_time}"
-				)
+                    f"FRET fetched on: {check_time}. Next check will be on: {upcoming_check_time}"
+                )
 
                 run_fetch = False
                 continue
 
-			# When the next check time has passed, toggle the run fetch variable and update the check time
+            # When the next check time has passed, toggle the run fetch variable and update the check time
             if datetime.now() >= upcoming_check_time:
                 check_time = datetime.now()
                 upcoming_check_time = check_time + check_interval
                 run_fetch = True
                 logger.info(
-					f"FRET fetched on: {check_time}. Next check will be on: {upcoming_check_time}"
-				)
+                    f"FRET fetched on: {check_time}. Next check will be on: {upcoming_check_time}"
+                )
             else:
-				# Wait for one minute before checking again.
+                # Wait for one minute before checking again.
                 time.sleep(60)
     except KeyboardInterrupt:
-		# When fetching is complete, gather historical data for evaluation.
+        # When fetching is complete, gather historical data for evaluation.
 
-		# Calculate correct end date as actual data is only reported up three days prior
+        # Calculate correct end date as actual data is only reported up three days prior
         final_fetch_time_api_format = deepcopy(check_time)
         final_fetch_time_api_format -= timedelta(days=4)
-		# Lastly, update date format so it's proper api date format.
+        # Lastly, update date format so it's proper api date format.
         final_fetch_time_api_format = final_fetch_time_api_format.strftime("%Y-%m-%d")
 
-        logger.info(f'Now getting historical data up to: {final_fetch_time_api_format}')
+        logger.info(f"Now getting historical data up to: {final_fetch_time_api_format}")
 
-        historical_arg_et = ETArg("actual_et", args={
-			"endpoint": timeseries_endpoint,
-			"variable": "ET",
-			"date_range": ["2016-01-01", final_fetch_time_api_format]
-		})
-        historical_arg_eto = ETArg("actual_eto", args={
-			"endpoint": timeseries_endpoint,
-			"variable": "ETo",
-			"date_range": ["2016-01-01", final_fetch_time_api_format]
-		})
-        historical_arg_etof = ETArg("actual_etof", args={
-			"endpoint": timeseries_endpoint,
-			"variable": "ETof",
-			"date_range": ["2016-01-01", final_fetch_time_api_format]
-		})
+        historical_arg_et = ETArg(
+            "actual_et",
+            args={
+                "endpoint": timeseries_endpoint,
+                "variable": "ET",
+                "date_range": ["2016-01-01", final_fetch_time_api_format],
+           		"reducer": "mean"
+            },
+        )
+        historical_arg_eto = ETArg(
+            "actual_eto",
+            args={
+                "endpoint": timeseries_endpoint,
+                "variable": "ETo",
+                "date_range": ["2016-01-01", final_fetch_time_api_format],
+            	"reducer": "mean"
+            },
+        )
+        historical_arg_etof = ETArg(
+            "actual_etof",
+            args={
+                "endpoint": timeseries_endpoint,
+                "variable": "ETof",
+                "date_range": ["2016-01-01", final_fetch_time_api_format],
+            	"reducer": "mean"
+            },
+        )
 
         logger.info("Fetching Monterey County historical data.")
-        mo_historical_fetch = ETPreprocess(monterey_queue, monterey_fields, api_key=api_key) # type: ignore
-        mo_historical_fetch.start(request_args=[historical_arg_et, historical_arg_eto, historical_arg_etof], frequency="daily", packets=True, logger=logger)
+        mo_historical_fetch = ETPreprocess(
+            monterey_queue, monterey_fields, api_key=api_key
+        )  # type: ignore
+        mo_historical_fetch.start(
+            request_args=[historical_arg_et, historical_arg_eto, historical_arg_etof],
+            frequency="daily",
+            packets=True,
+            logger=logger,
+        )
         mo_historical_fetch.export("data/monterey_historical.csv")
 
         logger.info("Fetching Kern County historical data.")
-        ke_historical_fetch = ETPreprocess(kern_queue, kern_fields, api_key=api_key) # type: ignore
-        ke_historical_fetch.start(request_args=[historical_arg_et, historical_arg_eto, historical_arg_etof], frequency="daily", packets=True, logger=logger)
+        ke_historical_fetch = ETPreprocess(kern_queue, kern_fields, api_key=api_key)  # type: ignore
+        ke_historical_fetch.start(
+            request_args=[historical_arg_et, historical_arg_eto, historical_arg_etof],
+            frequency="daily",
+            packets=True,
+            logger=logger,
+        )
         ke_historical_fetch.export("data/kern_historical.csv")
 
 if __name__ == "__main__":
-    main()
+	main()
--- polygon_fetch.py
+++ polygon_fetch.py
@@ -4,6 +4,7 @@ Created on Mon Sep 16 8:42:18 2024
 
 @author: Robin Fishman
 """
+
 from dotenv import dotenv_values
 
 import gzip
@@ -14,35 +15,59 @@ import requests
 boundaries_endpoint = "https://developer.openet-api.org/geodatabase/metadata/boundaries"
 
 api_key = dotenv_values(".env").get("ET_KEY")
-kern_fields = pd.read_csv("./data/Kern.csv", low_memory=False).sample(50)
-monterey_fields = pd.read_csv("./data/Monterey.csv", low_memory=False).sample(50)
+kern_fields = pd.read_csv("./data/Kern.csv", low_memory=False)
+monterey_fields = pd.read_csv("./data/Monterey.csv", low_memory=False)
 
-def get_polygons(fields, export, field_ref) -> pd.DataFrame|None:
-	res = requests.post(boundaries_endpoint,
-			headers={"Content-Type": "application/json", "Authorization": api_key},
-			json={"field_ids": fields})
-	
-	data = eval(gzip.decompress(res.content).decode())
-	df = pd.DataFrame(columns=["field_id", "CROP_2023", ".geo"])
-	for feat in data["features"]:
-		str_format_id = 'CA_' + feat['properties']['field_id'][2:]
-		crop = field_ref[field_ref['OPENET_ID'] == feat['properties']['field_id']]['CROP_2023'].values[0]
-		feat['geometry']['coordinates'] = [x for f in feat['geometry']['coordinates'] for s in f for x in s]
-		df = pd.concat([pd.DataFrame([[str_format_id, crop, feat['geometry']]], columns=df.columns), df], ignore_index=True)
-	with open(export, 'w') as file:
-		json.dump(data, file, ensure_ascii=False, indent=4)
-	
-	return df
+
+def get_polygons(fields, export, field_ref) -> pd.DataFrame | None:
+    res = requests.post(
+        boundaries_endpoint,
+        headers={"Content-Type": "application/json", "Authorization": api_key},
+        json={"field_ids": fields},
+    )
+
+    data = eval(gzip.decompress(res.content).decode())
+    df = pd.DataFrame(columns=["OPENET_ID", "CROP_2023", ".geo"])
+    for feat in data["features"]:
+        str_format_id = "CA_" + feat["properties"]["field_id"][2:]
+        crop = field_ref[field_ref["OPENET_ID"] == feat["properties"]["field_id"]][
+            "CROP_2023"
+        ].values[0]
+        feat["geometry"]["coordinates"] = [
+            x for f in feat["geometry"]["coordinates"] for s in f for x in s
+        ]
+        df = pd.concat(
+            [
+                pd.DataFrame(
+                    [[str_format_id, crop, feat["geometry"]]], columns=df.columns
+                ),
+                df,
+            ],
+            ignore_index=True,
+        )
+    with open(export, "w") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+    return df
+
 
 def main():
-	monterey_fields['OPENET_ID'] = '06' + monterey_fields['OPENET_ID'].str.slice(start=3)
-	kern_fields['OPENET_ID'] = '06' + kern_fields['OPENET_ID'].str.slice(start=3)
+    monterey_fields["OPENET_ID"] = "06" + monterey_fields["OPENET_ID"].str.slice(
+        start=3
+    )
+    kern_fields["OPENET_ID"] = "06" + kern_fields["OPENET_ID"].str.slice(start=3)
 
-	get_polygons(kern_fields['OPENET_ID'].to_list(), "./data/geo/kern_polygons_large.geojson", kern_fields).to_csv('data/kern_polygons_large.csv', index=False)
-	get_polygons(monterey_fields['OPENET_ID'].to_list(), "./data/geo/monterey_polygons_large.geojson", monterey_fields).to_csv('data/monterey_polygons_large.csv', index=False)
-	
-	kern_fields.to_csv('./data/random_kern.csv', index=False)
-	monterey_fields.to_csv('./data/random_monterey.csv', index=False)
+    get_polygons(
+        kern_fields["OPENET_ID"].to_list(),
+        "./data/geo/kern_polygons.geojson",
+        kern_fields,
+    ).to_csv("data/kern_polygons.csv", index=False)
+    get_polygons(
+        monterey_fields["OPENET_ID"].to_list(),
+        "./data/geo/monterey_polygons.geojson",
+        monterey_fields,
+    ).to_csv("data/monterey_polygons.csv", index=False)
+
 
 if __name__ == "__main__":
-	main()
+    main()
