ee8b66e953671acf0c4936174cb22180556e0450
Robin Fishman <rfishman@csumb.edu>
2025-02-03 23:43:32 +0000

    [HUC8] Convert HUC8 util functions to class.

--- huc8_locator.py
+++ huc8_locator.py
@@ -23,74 +23,75 @@ KEY = dotenv_values('.env').get('ET_KEY')
 
 dataset = ee.FeatureCollection('USGS/WBD/2017/HUC08')
 
-def get_huc8_metadata(huc8_id) -> pd.DataFrame:
-    # Filter huc8 IDs to return element matching ID provided.
-    elements: ee.Collection = dataset.filter(ee.Filter.eq('huc8', huc8_id))
-    # Localize.
-    info = elements.getInfo()
-    
-    # Extract coordinates from dataset element.
-    boundaries_raw = [x["geometry"]["coordinates"] for x in info["features"]]
-    # Flatten coordinate list as float values.
-    boundaries = np.array(boundaries_raw).flatten().astype(float).tolist()
-    
-    # Request Handler for getting Field IDs from geometry.
-    id_req = ETRequest(request_endpoint=endpoints['fieldId'],
-                    request_params= {
-                        "geometry": boundaries
-                    }, key=KEY)
-    # Queue request.
-    id_res = id_req.send()
-    
-    # Unzip data. List of Field IDs.
-    field_Ids = eval(gzip.decompress(id_res.content).decode())
-    
-    # Request Handler for getting field metadata.
-    metadata_req = ETRequest(request_endpoint=endpoints['fieldProps'],
-                            request_params={
-                                "field_ids": field_Ids
-                            }, key=KEY)
-    metadata_res = metadata_req.send()
-    
-    # Unzips data. List of Dicts['field_id', 'hectares', 'crop_2016', ..., 'crop_2022', ...]
-    metadata = eval(gzip.decompress(metadata_res.content).decode())
-    
-    # Convert to pandas dataframe.
-    df = pd.DataFrame.from_records(metadata)
-    
-    return df
+class HUC8:
+    def get_huc8_metadata(huc8_id) -> pd.DataFrame:
+        # Filter huc8 IDs to return element matching ID provided.
+        elements: ee.Collection = dataset.filter(ee.Filter.eq('huc8', huc8_id))
+        # Localize.
+        info = elements.getInfo()
+        
+        # Extract coordinates from dataset element.
+        boundaries_raw = [x["geometry"]["coordinates"] for x in info["features"]]
+        # Flatten coordinate list as float values.
+        boundaries = np.array(boundaries_raw).flatten().astype(float).tolist()
+        
+        # Request Handler for getting Field IDs from geometry.
+        id_req = ETRequest(request_endpoint=endpoints['fieldId'],
+                        request_params= {
+                            "geometry": boundaries
+                        }, key=KEY)
+        # Queue request.
+        id_res = id_req.send()
+        
+        # Unzip data. List of Field IDs.
+        field_Ids = eval(gzip.decompress(id_res.content).decode())
+        
+        # Request Handler for getting field metadata.
+        metadata_req = ETRequest(request_endpoint=endpoints['fieldProps'],
+                                request_params={
+                                    "field_ids": field_Ids
+                                }, key=KEY)
+        metadata_res = metadata_req.send()
+        
+        # Unzips data. List of Dicts['field_id', 'hectares', 'crop_2016', ..., 'crop_2022', ...]
+        metadata = eval(gzip.decompress(metadata_res.content).decode())
+        
+        # Convert to pandas dataframe.
+        df = pd.DataFrame.from_records(metadata)
+        
+        return df
 
-def get_timeseries_data(field_ids) -> pd.DataFrame:
-    # Request Handler for timeseries data.
-    req = ETRequest(request_endpoint=endpoints['timeseries'],
-                    request_params={
-                        "date_range": ["2022-01-01", "2022-12-31"],
-                        "interval": "monthly",
-                        "field_ids": field_ids,
-                        "models": [
-                            "Ensemble",
-                            "geeSEBAL",
-                            "SSEBop",
-                            "SIMS",
-                            "DisALEXI",
-                            "PTJPL",
-                            "eeMetric"
-                        ],
-                        "variables": ["ETof"],
-                        "file_format": "JSON"
-                    }, key=KEY)
-    res = req.send()
-    
-    data = eval(gzip.decompress(res.content).decode())
-    
-    df = pd.DataFrame(data)
-    
-    return df
+    def get_timeseries_data(field_ids) -> pd.DataFrame:
+        # Request Handler for timeseries data.
+        req = ETRequest(request_endpoint=endpoints['timeseries'],
+                        request_params={
+                            "date_range": ["2022-01-01", "2022-12-31"],
+                            "interval": "monthly",
+                            "field_ids": field_ids,
+                            "models": [
+                                "Ensemble",
+                                "geeSEBAL",
+                                "SSEBop",
+                                "SIMS",
+                                "DisALEXI",
+                                "PTJPL",
+                                "eeMetric"
+                            ],
+                            "variables": ["ETof"],
+                            "file_format": "JSON"
+                        }, key=KEY)
+        res = req.send()
+        
+        data = eval(gzip.decompress(res.content).decode())
+        
+        df = pd.DataFrame(data)
+        
+        return df
 
 def main():
     huc8Id = sys.argv[1]
     # Gets metadata from field IDs found in HUC8 boundary.
-    metadata = get_huc8_metadata(huc8Id)
+    metadata = HUC8.get_huc8_metadata(huc8Id)
     # metadata.to_csv(f"./data/huc8/{huc8Id}_metadata.csv")
     
     top_crops = metadata['crop_2022'].value_counts(ascending=False)[:3]
@@ -98,7 +99,7 @@ def main():
     collecting_fields = metadata[metadata['crop_2022'].isin(top_crops.index.to_list())]
     
     start = time.perf_counter()
-    data = get_timeseries_data(collecting_fields['field_id'].astype(str).tolist())
+    data = HUC8.get_timeseries_data(collecting_fields['field_id'].astype(str).tolist())
     stop = time.perf_counter()
     
     # data.to_csv(f"./data/huc8/{huc8Id}_values.csv")
