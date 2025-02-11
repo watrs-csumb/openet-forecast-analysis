51a6bb44f045fa3ace4189fe53ac9d17e1909908
Robin Fishman <rfishman@csumb.edu>
2025-02-11 00:08:57 +0000

    [HUC8] Add flags for filtering peak season and expanded EToF max (Kcp) crop filtering.

--- huc8_locator.py
+++ huc8_locator.py
@@ -46,8 +46,9 @@ parser.add_argument("-huc8", "--huc8", nargs=OPTIONAL, type=str, help="HUC8 Code
 parser.add_argument("-y", "--year", nargs=OPTIONAL, default="2022", help="Year of Reference. Default, 2022")
 parser.add_argument("-e", "--exclude", nargs=OPTIONAL, default=[], type=list, help="List of USDS CDL codes to exclude for EToF maxes")
 parser.add_argument("-d", "--dest", nargs=OPTIONAL, default=None, help="Dir/File prefix for output. Defaults to huc8 code")
+parser.add_argument("-t", "--top", nargs=OPTIONAL, default=3, type=int, help="Number of top crops to fetch for each watershed. To include all, enter 0. Default, 3")
+parser.add_argument("-p", "--peak", nargs=2, default=(4,8), type=tuple, help="Start and end months of peak season. Default, (4,8)")
 parser.add_argument("-k", "--key", help="OpenET API Key")
-# add verbose flag
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -117,6 +118,12 @@ def get_huc8_metadata(huc8_id: str, api_key: str) -> Optional[pd.DataFrame]:
     if id_res is None:
         return
     
+    # Check if request was successful.
+    if id_res.status_code != 200:
+        print(f"Error {id_res.status_code}: {id_res.text}")
+        sys.exit(1)
+        return
+    
     # Unzip data. List of Field IDs.
     field_Ids = eval(gzip.decompress(id_res.content).decode())
     
@@ -129,6 +136,12 @@ def get_huc8_metadata(huc8_id: str, api_key: str) -> Optional[pd.DataFrame]:
     )
     
     if metadata_res is None:
+        return
+    
+    # Check if request was successful.
+    if metadata_res.status_code != 200:
+        print(f"Error {metadata_res.status_code}: {metadata_res.text}")
+        sys.exit(1)
         return
     
     # Unzips data. List of Dicts["field_id", "hectares", "crop_2016", ..., "crop_2022", ...]
@@ -165,6 +178,13 @@ def get_timeseries_data(field_ids: List[str], api_key: str, year: Union[str, int
     
     if res is None:
         return
+    
+    # Check if request was successful.
+    if res.status_code != 200:
+        print(f"Error {res.status_code}: {res.text}")
+        sys.exit(1)
+        return
+    
     data = eval(gzip.decompress(res.content).decode())
 
     df = pd.DataFrame(data)
@@ -179,6 +199,20 @@ def main():
     filename = args.dest or huc8Id
     api_key = args.key
     crop_excludes = args.exclude
+    n_crops = args.top
+    s_peak, e_peak = args.peak
+    
+    if n_crops < 0:
+        print("Number of crops cannot be negative.")
+        sys.exit(1)
+    
+    if s_peak not in range(1, 13) or e_peak not in range(1, 13):
+        print("Months must be between 1 and 12.")
+        sys.exit(1)
+    
+    if e_peak < s_peak:
+        print("End of peak season cannot be before start of peak season.")
+        sys.exit(1)
     
     # If filename is a directory, append HUC8 ID to it.
     if pathlib.Path(filename).is_dir():
@@ -197,31 +231,34 @@ def main():
         print(f"{year} is not present in field metadata.")
         return
     
-    metadata.to_csv(f"{filename}_metadata_{year}.csv")
+    metadata_loc = f"{filename}_metadata_{year}.csv"
+    metadata.to_csv(metadata_loc)
+    print(f"Exported metadata to {metadata_loc}")
     
-    print("Trimming fields for top 3 crops...")
-    # Goes through list of crops to exclude.
-    excluder = metadata[~metadata[crop_col].isin(crop_excludes)]
-    # Grabs the top 3 crops for the given year.
-    top_crops = excluder[crop_col].value_counts(ascending=False)[:3]
     
-    collecting_fields = excluder[
-        excluder[crop_col].isin(top_crops.index.to_list())
-    ]
+    if n_crops > 0:
+        print(f"Trimming fields for top {n_crops} crops...")
+        # Goes through list of crops to exclude.
+        excluder = metadata[~metadata[crop_col].isin(crop_excludes)]
+        # Grabs the top crops for the given year.
+        top_crops = excluder[crop_col].value_counts(ascending=False)[:n_crops].index.to_list()
+        
+        metadata = excluder[excluder[crop_col].isin(top_crops)]
     
     start = time.perf_counter()
-    data = get_timeseries_data(collecting_fields["field_id"].astype(str).tolist(), api_key=api_key, year=year)
+    data = get_timeseries_data(metadata["field_id"].astype(str).tolist(), api_key=api_key, year=year)
     stop = time.perf_counter()
     
     if data is None:
+        print("Failed to get timeseries data.")
         return None
     
     data.to_csv(f"{filename}_values_{year}.csv")
     
-    print(f"{collecting_fields['field_id'].agg('count')} fields took {round((stop-start), 2)} seconds.")
+    print(f"{metadata['field_id'].agg('count')} fields took {round((stop-start), 2)} seconds.")
     
-    crops_max = data.groupby(["field_id", "collection"])["value_mm"].max()
-    crops_max.reset_index().to_csv(f"{filename}_etof_maxes_{year}.csv")
+    crops_max = data.groupby(["field_id", "collection"])["value_mm"].max().round(2)
+    crops_max.reset_index().to_csv(f"{filename}_etof_maxes_{year}.csv", index=False)
     
     return
 
