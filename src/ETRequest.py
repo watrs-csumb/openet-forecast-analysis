4237c7c32e38a1860b7b3bb95e1f39e43f7a4cbd
Robin Fishman <13447870+aetriusgx@users.noreply.github.com>
2025-04-23 18:12:04 +0000

    Request strategy #3

--- src/ETRequest.py
+++ src/ETRequest.py
@@ -1,37 +1,17 @@
-import contextlib
-import threading
 import time
 import warnings
 
 from logging import Logger, WARNING, ERROR, addLevelName
 from requests import Response, post
-from requests.exceptions import Timeout
+from requests.exceptions import Timeout, ConnectionError
+
+from .ETException import ETException
 
 STATUS_ALLOWED = [200]
 TIMEOUT = 60 * 5
 
 HELPFUL = 25
 addLevelName(HELPFUL, "HELPFUL")
-
-INTERRUPT_N = 0
-INTERRUPT_LIMIT = 2
-interruptlock = threading.Lock()
-
-@contextlib.contextmanager
-def interrupt_handler(): 
-    global INTERRUPT_N, INTERRUPT_LIMIT
-    with interruptlock:
-        INTERRUPT_N = 0
-    try:
-        yield
-    except KeyboardInterrupt:
-        with interruptlock:
-            INTERRUPT_N += 1
-        if INTERRUPT_N >= INTERRUPT_LIMIT:
-            raise
-        else:
-            print("KeyboardInterrupt detected. Press Ctrl+C again to quit.")
-            raise KeyboardInterrupt # Workround so this handler doesn't need a larger scope and still letting the send() function handle the KeyboardInterrupt.
 
 class Request:
     def __init__(
@@ -49,90 +29,64 @@ class Request:
         self._attempt: int = 1
         self.response: Response | None = None
 
-    def _retry(self, attempts: int) -> None:
-        while not self.success() and self._attempt <= attempts:
-            self._log(WARNING, f"Reattempting request ({self._attempt}/{attempts})..")
-            
-            sleep_time = (2**self._attempt) % 10
-            time.sleep(sleep_time)
-            self._attempt += 1
-            self.response = self.send()
-
-    def _log(self, level: int, message: str, **kwargs) -> None:
-        if not self.logger or not isinstance(self.logger, Logger):
-            return
-
-        self.logger.log(level, message, **kwargs)
-
-    def send(
-        self, max_retries: int = 3, ignore_fails: bool = False
-    ) -> Response | None:
+    def _retry_request(self, n_retries: int = 3) -> None:
         if not self.endpoint:
-            raise AttributeError("Request has no endpoint.")
+            raise AttributeError("No endpoint provided for request.")
         if not self.params:
-            raise AttributeError("Request has no parameters.")
-        if not self.header["Authorization"]:
-            raise AttributeError("Request has no API key.")
+            raise AttributeError("No parameters provided for request.")
+        if isinstance(self.params, dict) and len(self.params.keys()) == 0:
+            raise AttributeError("Request parameters cannot be empty.")
+        if not self.header.get("Authorization", None):
+            raise AttributeError("No Authorization key provided for request.")
         
-        try:
-            with interrupt_handler() as interrupted:
-                self.response = post(
+        res = None
+        err = None
+        
+        for attempt in range(self._attempt, n_retries + 1):
+            if attempt > 1:
+                # Exponential backoff
+                time.sleep(min(2 ** (attempt - 1), 60))
+            try:
+                res = post(
                     url=self.endpoint,
                     json=self.params,
                     headers=self.header,
-                    timeout=TIMEOUT,
+                    timeout=TIMEOUT
                 )
                 
-            if interrupted or INTERRUPT_N >= INTERRUPT_LIMIT:
-                return
-
-            if not ignore_fails and not self.success():
-                if self.response:
-                    self._log(HELPFUL, f"Response[{self.response.status_code}]: {self.response.text}")
-                raise ValueError("Request did not succeed with a 200 status code.")
-
-        except KeyboardInterrupt:
-            ignore_fails = True
-            return
-
-        except AttributeError as err:
-            self._log(ERROR, f"Request failed.\n{err}")
-            ignore_fails = True
-            return
-
-        except Timeout:
-            # Automatically reattempts after timeout.
-            self._log(ERROR, "Request timed out.")
-            self._retry(max_retries)
-
-        except Exception as err:
-            self._log(ERROR, f"Request failed.\n{err}")
-            self._retry(max_retries)
-
-        finally:
-            if not self.success() and not ignore_fails:
-                req_feedback = ""
-                if not self.response:
-                    req_feedback = "Received no response from server. Please check your internet connection."
-                else:
-                    status = self.response.status_code
-                    msg = self.response.text
-                    req_feedback = f"Request failed with status code {status}. {msg}"
-
-                self._log(ERROR, req_feedback)
-                self._log(HELPFUL, str(self.params))
-
-                will_reattempt = input(f"{req_feedback}\nReattempt request? (Y/n): ")
-
-                if will_reattempt.lower() in ["y", ""]:
-                    return self.send()
-
-            return self.response
-
-    def success(self) -> bool:
-        # Returns true in the event that a response is returned and its status code is in STATUS_ALLOWED.
+                if not self.success(res):
+                    raise ValueError()
+                
+            except (Timeout, ConnectionError, ValueError) as e:
+                err = e
+            
+            if err is not None:
+                if attempt == n_retries:
+                    raise ETException
+                
+                if self.logger:
+                    self.logger.log(
+                        ERROR,
+                        f"Attempt {attempt} failed for endpoint {self.endpoint}: {err}"
+                    )
+                continue
+        
+        self.response = res
+    
+    def send(self, n_retries: int = 3) -> Response | None:
         try:
-            return self.response.status_code in STATUS_ALLOWED  # type: ignore
+            self._retry_request(n_retries)
+        except ETException:
+            return None
+        
+        return self.response
+
+    def success(self, request: Response | None = None) -> bool:
+        # Returns true in the event that a response is returned and its status code is in STATUS_ALLOWED.
+        req = request or self.response
+        
+        try:
+            return req.status_code in STATUS_ALLOWED  # type: ignore
         except Exception:
             return False
 
